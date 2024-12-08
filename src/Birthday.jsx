1b8d36aa46775c8ee4a33c36af881d4c260ee9d7
Evgeniy Kolmak <zenakolmak669@gmail.com>
2024-12-08 19:49:29 +0300

    change properties

--- src/Birthday.jsx
+++ src/Birthday.jsx
@@ -15,9 +15,9 @@ const Birthday = ({ name, day, month }) => {
 
   if (name === undefined || day === undefined || month === undefined) {
     // This is if not enough params are provided
-    name = 'Deepankar'; // Name of the Person
+    name = 'Evgeniy'; // Name of the Person
     month = 6; // Month of the Birthday
-    day = 14; // Day of the Birthday
+    day = 6; // Day of the Birthday
   }
 
   // get current time
